b8d4b33e4dd93432c9509f44e4a06dbce63c4eee
BintangAsyidqy <muhammadbintangasyidqy@smkwikrama.sch.id>
2025-01-21 10:01:12 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" type="image/png" href="img/fav.png">
+      <title>Job Portal</title>
+      <!-- Slick Slider -->
+      <link rel="stylesheet" type="text/css" href="vendor/slick/slick.min.css"/>
+      <link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.min.css"/>
+      <!-- Feather Icon-->
+      <link href="vendor/icons/feather.css" rel="stylesheet" type="text/css">
+      <!-- Bootstrap core CSS -->
+      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Custom styles for this template -->
+      <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+   </head>
+   <body>
+      <div class="bg-white">
+         <div class="container">
+            <div class="row justify-content-center align-items-center d-flex vh-100">
+               <div class="col-md-4 mx-auto">
+                  <div class="osahan-login py-4">
+                     <div class="text-center mb-4">
+                        <a href="index.html"><img src="img/logo.svg" alt=""></a>
+                        <h5 class="font-weight-bold mt-3">Welcome Back</h5>
+                        <p class="text-muted">Don't miss your next opportunity. Sign in to stay updated on your professional world.</p>
+                     </div>
+                     <form action="index.html">
+                        <div class="form-group">
+                           <label class="mb-1">Email or Phone</label>
+                           <div class="position-relative icon-form-control">
+                              <i class="feather-user position-absolute"></i>
+                              <input type="email" class="form-control">
+                           </div>
+                        </div>
+                        <div class="form-group">
+                           <label class="mb-1">Password</label>
+                           <div class="position-relative icon-form-control">
+                              <i class="feather-unlock position-absolute"></i>
+                              <input type="password" class="form-control">
+                           </div>
+                        </div>
+                        <div class="custom-control custom-checkbox mb-3">
+                           <input type="checkbox" class="custom-control-input" id="customCheck1">
+                           <label class="custom-control-label" for="customCheck1">Remember password</label>
+                        </div>
+                        <button class="btn btn-primary btn-block text-uppercase" type="submit"> Sign in </button>
+                        <div class="text-center mt-3 border-bottom pb-3">
+                           <p class="small text-muted">Or login with</p>
+                           <div class="row">
+                              <div class="col-4">
+                                 <button type="button" class="btn btn-sm btn-outline-instagram btn-block"><i class="feather-instagram"></i> Instagram</button>
+                              </div>
+                              <div class="col-4">
+                                 <button type="button" class="btn btn-sm btn-outline-linkedin btn-block"><i class="feather-linkedin"></i> Linkedin</button>
+                              </div>
+                              <div class="col-4">
+                                 <button type="button" class="btn btn-sm btn-outline-facebook btn-block"><i class="feather-facebook"></i> Facebook</button>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="py-3 d-flex align-item-center">
+                           <a href="forgot-password.html">Forgot password?</a>
+                           <span class="ml-auto"> New to Osahanin? <a class="font-weight-bold" href="sign-up.html">Join now</a></span>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Bootstrap core JavaScript -->
+      <script src="vendor/jquery/jquery.min.js" type="636623405dcacec7990beac1-text/javascript"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js" type="636623405dcacec7990beac1-text/javascript"></script>
+      <!-- slick Slider JS-->
+      <script type="636623405dcacec7990beac1-text/javascript" src="vendor/slick/slick.min.js"></script>
+      <!-- Custom scripts for all pages-->
+      <script src="js/osahan.js" type="636623405dcacec7990beac1-text/javascript"></script>
+   <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="636623405dcacec7990beac1-|49" defer></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"9053b8255a42449f","version":"2025.1.0","r":1,"token":"dd471ab1978346bbb991feaa79e6ce5c","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}' crossorigin="anonymous"></script>
+</body>
+</html>
