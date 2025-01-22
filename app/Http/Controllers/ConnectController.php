e8f66577a75037ac99336647ca5c2c6b33ac4c38
BintangAsyidqy <muhammadbintangasyidqy@smkwikrama.sch.id>
2025-01-22 10:50:57 +0700

    update

--- app/Http/Controllers/ConnectController.php
+++ app/Http/Controllers/ConnectController.php
@@ -2,64 +2,145 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\cr;
 use Illuminate\Http\Request;
+use App\Models\User; // Pastikan model User diimport
 
 class ConnectController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        return view('home');
+    }
+    public function connect()
+    {
+        return view('connections.index');
+    }
+    // Mengembalikan halaman suggestions dengan data suggestedUsers yang statis
+    public function suggestions()
+    {
+        $suggestedUsers = [
+            [
+                'id' => 1,
+                'name' => 'Sophia Lee',
+                'position' => 'Photographer at Photography',
+                'image' => 'img/p1.png',
+                'mutual_connections' => 4
+            ]
+        ];
+        
+        return view('connections.suggestions', compact('suggestedUsers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // Mengembalikan halaman suggestions dengan data suggestedUsers dari database
+    public function showSuggestedUsers()
     {
-        //
+        $suggestedUsers = User::all(); // Ambil semua user dari database
+        return view('connections.suggestions', ['suggestedUsers' => $suggestedUsers]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function notification()
     {
-        //
+        return view('connections.notifications');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(cr $cr)
+    public function message()
     {
-        //
+        return view('connections.messages');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(cr $cr)
+    public function terms()
     {
-        //
+        return view('terms.term');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, cr $cr)
+    public function job()
     {
-        //
+        return view('job.jobs');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(cr $cr)
+    public function profile()
     {
-        //
+        return view('profile.profile');
+    }
+
+    public function companyProfile()
+    {
+        return view('profile.company-profile');
+    }
+
+    public function jobProfile()
+    {
+        return view('profile.job-profile');
+    }
+
+    public function notFound()
+    {
+        return view('error.not-found');
+    }
+
+    public function faq()
+    {
+        return view('faq.faq');
+    }
+
+    public function privacy()
+    {
+        return view('faq.privacy');
+    }
+
+    public function comingSoon()
+    {
+        return view('error.coming-soon');
+    }
+
+    public function maintenance()
+    {
+        return view('error.maintence');
+    }
+
+    public function blog()
+    {
+        return view('blog.blogs');
+    }
+
+    public function blogSingle()
+    {
+        return view('blog.blog-single');
+    }
+
+    public function components()
+    {
+        return view('pricings.components');
+    }
+
+    public function pricing()
+    {
+        return view('pricings.pricing');
+    }
+
+    public function contact()
+    {
+        return view('profile.contact');
+    }
+
+    public function signIn()
+    {
+        return view('login.sign-in');
+    }
+
+    public function signUp()
+    {
+        return view('login.sign-up');
+    }
+
+    public function forgotPassword()
+    {
+        return view('login.forgot-password');
+    }
+
+    public function editProfile()
+    {
+        return view('profile.edit-profile');
     }
 }
--- resources/views/login/sign-in.blade.php
+++ resources/views/login/sign-in.blade.php
@@ -6,7 +6,7 @@
       <meta name="description" content="">
       <meta name="author" content="">
       <link rel="icon" type="image/png" href="img/fav.png">
-      <title>Job Portal</title>
+      <title>Osahanin - Job Portal & Social Network HTML Template</title>
       <!-- Slick Slider -->
       <link rel="stylesheet" type="text/css" href="vendor/slick/slick.min.css"/>
       <link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.min.css"/>
@@ -15,7 +15,7 @@
       <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
       <!-- Custom styles for this template -->
-      <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
    </head>
    <body>
       <div class="bg-white">
@@ -24,11 +24,11 @@
                <div class="col-md-4 mx-auto">
                   <div class="osahan-login py-4">
                      <div class="text-center mb-4">
-                        <a href="index.html"><img src="img/logo.svg" alt=""></a>
+                        <a href="{{route('home')}}"><img src="img/logo.svg" alt=""></a>
                         <h5 class="font-weight-bold mt-3">Welcome Back</h5>
                         <p class="text-muted">Don't miss your next opportunity. Sign in to stay updated on your professional world.</p>
                      </div>
-                     <form action="index.html">
+                     <form action="{{route('connect.index')}}">
                         <div class="form-group">
                            <label class="mb-1">Email or Phone</label>
                            <div class="position-relative icon-form-control">
@@ -63,8 +63,8 @@
                            </div>
                         </div>
                         <div class="py-3 d-flex align-item-center">
-                           <a href="forgot-password.html">Forgot password?</a>
-                           <span class="ml-auto"> New to Osahanin? <a class="font-weight-bold" href="sign-up.html">Join now</a></span>
+                           <a href="{{route('forgot-password')}}">Forgot password?</a>
+                           <span class="ml-auto"> New to Osahanin? <a class="font-weight-bold" href="{{route('sign-up')}}">Join now</a></span>
                         </div>
                      </form>
                   </div>
@@ -73,12 +73,11 @@
          </div>
       </div>
       <!-- Bootstrap core JavaScript -->
-      <script src="vendor/jquery/jquery.min.js" type="636623405dcacec7990beac1-text/javascript"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js" type="636623405dcacec7990beac1-text/javascript"></script>
+      <script src="vendor/jquery/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
       <!-- slick Slider JS-->
-      <script type="636623405dcacec7990beac1-text/javascript" src="vendor/slick/slick.min.js"></script>
+      <script type="text/javascript" src="vendor/slick/slick.min.js"></script>
       <!-- Custom scripts for all pages-->
-      <script src="js/osahan.js" type="636623405dcacec7990beac1-text/javascript"></script>
-   <script src="/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js" data-cf-settings="636623405dcacec7990beac1-|49" defer></script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015" integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ==" data-cf-beacon='{"rayId":"9053b8255a42449f","version":"2025.1.0","r":1,"token":"dd471ab1978346bbb991feaa79e6ce5c","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}}}' crossorigin="anonymous"></script>
-</body>
+      <script src="js/osahan.js"></script>
+   </body>
 </html>
